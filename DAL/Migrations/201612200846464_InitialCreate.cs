7268f30e98df90215786d4011bba5d9dcc430125
Pavel_Yakimovich <Pavel_Yakimovich@EPBYMINW2323>
2016-12-20 12:21:10 +0300

    initial commit

---
+++ DAL/Migrations/201612200846464_InitialCreate.cs
@@ -0,0 +1,28 @@
+namespace DAL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.ToDoes",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Completed = c.Boolean(nullable: false),
+                        Task = c.String(),
+                        UserId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.ToDoes");
+        }
+    }
+}
---
+++ DAL/Repositories/ToDoRepository.cs
@@ -0,0 +1,54 @@
+﻿using DAL.ORM;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+
+namespace DAL.Repositories
+{
+    public class ToDoRepository : IDisposable
+    {
+        private DbContext _dataBase;
+
+        public ToDoRepository (DbContext context)
+        {
+            _dataBase = context;
+        }
+
+        public IEnumerable<ToDo> GetAll()
+        {
+            return _dataBase.Set<ToDo>().Select(x => x);
+        }
+
+        public bool Create(ToDo toDo)
+        {
+            _dataBase.Set<ToDo>().Add(toDo);
+            return _dataBase.SaveChanges() > 0;
+        }
+
+        public bool Update(ToDo toDo)
+        {
+            _dataBase.Entry(toDo).State = EntityState.Modified; // if there are changes
+            return _dataBase.SaveChanges() > 0;
+        }
+
+        public bool Delete(int id)
+        {
+            var entity = _dataBase.Set<ToDo>().Find(id);
+
+            if (ReferenceEquals(entity, null))
+            {
+                return false;
+            }
+
+            _dataBase.Set<ToDo>().Remove(entity);
+            return _dataBase.SaveChanges() > 0;
+        }
+
+        public void Dispose()
+        {
+            _dataBase.Dispose();
+        }
+    }
+}
